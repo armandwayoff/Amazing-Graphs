061b9b0e30e5ffee5f7ab2ba86e9117bfaf569ce
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-05-13 10:50:55 +0200

    Update and rename A005185 - Suite Q d'Hofstadter/hofstadter.py to A005185 - Hofstadter's Q Sequence/hofstadter.py

--- A005185 - Hofstadter's Q Sequence/hofstadter.py
+++ A005185 - Hofstadter's Q Sequence/hofstadter.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2]
 y = [1, 1]
 
 for i in range(2, 10000):
-    x.append(i + 1)
     y.append(y[i - y[i - 1]] + y[i - y[i - 2]])
 
+x = range(1, len(y) + 1)
 plt.scatter(x, y, s=0.1)
 plt.xlabel('n')
 plt.ylabel('A005185(n)')
