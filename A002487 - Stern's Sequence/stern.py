92c6000a89330396b4bdbf0a6f154bba7d500a02
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-05-13 10:38:35 +0200

    Create stern.py

---
+++ A002487 - Stern's Sequence/stern.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+y = [0, 1]
+
+for iter in range(15):
+    newY = []
+    for i in range(len(y) - 1):
+        newY.append(y[i])
+        newY.append(y[i] + y[i + 1])
+    newY.append(1)
+    y = newY
+
+x = range(len(y))
+plt.scatter(x, y, s=0.5)
+plt.xlabel('n')
+plt.ylabel('A002487(n)')
+plt.title("A002487 - Suite diatomique de Stern")
+plt.show()
