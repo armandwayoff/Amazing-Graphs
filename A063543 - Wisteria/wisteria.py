5e66150399416759990538f737fb770d07bc4882
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-05-13 11:07:30 +0200

    Code optimization

--- A063543 - Wisteria/wisteria.py
+++ A063543 - Wisteria/wisteria.py
@@ -1,17 +1,16 @@
 import matplotlib.pyplot as plt
 
-x = []
 y = []
 
 for i in range(1, 2000):
-    chiffres = map(int, str(i))
-    produit = 1
-    for c in chiffres:
-        if c != 0:
-            produit *= c
-    x.append(i)
-    y.append(i - produit)
+    digits = map(int, str(i))
+    product = 1
+    for d in digits:
+        if d != 0:
+            product *= d
+    y.append(i - product)
 
+x = range(1, len(y) + 1)
 plt.scatter(x, y, s=0.3)
 plt.xlabel('n')
 plt.ylabel('A063543(n)')
