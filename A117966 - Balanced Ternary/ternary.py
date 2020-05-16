8d58e648b09a639fcf19299938f07e89e5987918
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-05-16 12:30:44 +0200

    Create ternary.py

---
+++ A117966 - Balanced Ternary/ternary.py
@@ -0,0 +1,26 @@
+# Code by Indranil Ghosh (https://oeis.org/wiki/User:Indranil_Ghosh)
+
+import matplotlib.pyplot as plt
+
+
+def a(n):
+    if n == 0:
+        return 0
+    if n % 3 == 0:
+        return 3 * a(n / 3)
+    elif n % 3 == 1:
+        return 3 * a((n - 1) / 3) + 1
+    else:
+        return 3 * a((n - 2) / 3) - 1
+
+
+y = []
+for i in range(100000):
+    y.append(a(i))
+
+x = range(len(y))
+plt.scatter(x, y, s=0.1)
+plt.xlabel('n')
+plt.ylabel('A117966(n)')
+plt.title("A117966 - Balanced ternary")
+plt.show()
