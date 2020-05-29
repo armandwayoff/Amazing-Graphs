d62b0b494de3328f316f34663e34c62fdd47ca78
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-05-29 23:37:33 +0200

    Create A006577.py

---
+++ A006577 - Number of steps for n to reach 1/A006577.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+y = []
+
+for n in range(1, 10**4):
+    count = 0
+    while n > 1:
+        n = n / 2 if n % 2 == 0 else 3 * n + 1
+        count += 1
+    y.append(count)
+
+x = range(1, len(y) + 1)
+plt.scatter(x, y, s=0.5)
+plt.xlabel('n')
+plt.ylabel('A006577(n)')
+plt.title("A006577 - Number of steps for n to reach 1")
+plt.show()
