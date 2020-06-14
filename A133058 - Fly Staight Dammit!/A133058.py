df942bb9b4357af59f56b00f4237f96eb1ee4a26
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-06-15 00:18:54 +0200

    Update A133058.py

--- A133058 - Fly Staight Dammit!/A133058.py
+++ A133058 - Fly Staight Dammit!/A133058.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
-import math
+from math import gcd
 
 y = [1, 1]
 
-for n in range(2, 1000):
-    if math.gcd(n, y[n - 1]) == 1:
+for n in range(2, 10 ** 3):
+    if gcd(n, y[n - 1]) == 1:
         y.append(y[n - 1] + n + 1)
     else:
-        y.append(int(y[n - 1] / math.gcd(n, y[n-1])))
+        y.append(int(y[n - 1] / gcd(n, y[n-1])))
 
 x = range(len(y))
 plt.scatter(x, y, s=8)
