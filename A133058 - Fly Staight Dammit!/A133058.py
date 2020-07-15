1915f0492dca10e694b6c5cdfa9ff33f60018ce7
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-07-15 11:41:00 +0200

    Add plt.figure title

--- A133058 - Fly Staight Dammit!/A133058.py
+++ A133058 - Fly Staight Dammit!/A133058.py
@@ -10,8 +10,9 @@ for n in range(2, 10 ** 3):
         y.append(int(y[n - 1] / gcd(n, y[n-1])))
 
 x = range(len(y))
+plt.figure("A133058 - Fly straight, dammit")
 plt.scatter(x, y, s=8)
 plt.xlabel('n')
 plt.ylabel('A133058(n)')
-plt.title("A133058 - Fly Staight Dammit!")
+plt.title("A133058 - Fly straight, dammit")
 plt.show()
