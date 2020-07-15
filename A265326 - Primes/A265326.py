7611f4d762345aa1512120e5af2205c6662f2a48
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-07-15 15:01:07 +0200

    Add plt.figure title

--- A265326 - Primes/A265326.py
+++ A265326 - Primes/A265326.py
@@ -9,6 +9,7 @@ for i in range(10 ** 5):
         x.append(i)
         y.append(i - int(bin(i)[:1:-1], 2))
 
+plt.figure("A265326 - Primes")
 plt.scatter(x, y, s=0.5)
 plt.xlabel('n')
 plt.ylabel('A265326(n)')
