340eac089ad6bdc6c3b201fb2b89271094b54151
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-07-15 11:35:49 +0200

    Add plt.figure title

--- A005185 - Hofstadter's Q Sequence/A005185.py
+++ A005185 - Hofstadter's Q Sequence/A005185.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 
 y = [1, 1]
 
-for i in range(2, 10 ** 5):
+for i in range(2, 10 ** 4):
     y.append(y[i - y[i - 1]] + y[i - y[i - 2]])
 
 x = range(1, len(y) + 1)
+plt.figure("A005185 - Hofstadter's Q Sequence")
 plt.scatter(x, y, s=0.1)
 plt.xlabel('n')
 plt.ylabel('A005185(n)')
