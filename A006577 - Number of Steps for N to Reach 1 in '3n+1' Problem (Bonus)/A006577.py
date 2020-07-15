36baab09ad3a66bdb1e0858f54bcda07071a44bd
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-07-15 11:37:10 +0200

    Add plt.figure title

--- A006577 - Number of Steps for N to Reach 1 in '3n+1' Problem (Bonus)/A006577.py
+++ A006577 - Number of Steps for N to Reach 1 in '3n+1' Problem (Bonus)/A006577.py
@@ -10,6 +10,7 @@ for n in range(1, 10 ** 4):
     y.append(count)
 
 x = range(1, len(y) + 1)
+plt.figure("A006577 - Number of Steps for N to Reach 1 in '3n+1' Problem (Bonus)")
 plt.scatter(x, y, s=0.5)
 plt.xlabel('n')
 plt.ylabel('A006577(n)')
